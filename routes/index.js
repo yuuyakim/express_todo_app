7cd9d7f54d97367055b5ceea77e926e0c8e68c35
Yuuya Kimura <yuuya.kim0801@gmail.com>
2023-06-12 16:08:50 +0900

    DBへの保存、読み込み

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Rupura15",
+  database: "todo_app",
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ToDoApp' });
+router.get("/", function (req, res, next) {
+  connection.query(`select * from tasks;`, (error, results) => {
+    console.log(error);
+    console.log(results);
+    res.render("index", {
+      title: "ToDo App",
+      todos: results,
+    });
+  });
 });
 
 router.post("/", function (req, res, next) {
-  res.render("index", { title: "ToDoApp" });
+  connection.connect((err) => {
+    if (err) {
+      console.log("error connecting: " + err.stack)
+      return
+    }
+    console.log('success')
+  })
+  const todo = req.body.add;
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  )
 });
 
 module.exports = router;
