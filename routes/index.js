58d71497e3a790f3c84bfb141c883fa9a6fb1c91
Yuuya Kimura <yuuya.kim0801@gmail.com>
2023-06-13 13:52:27 +0900

    signin処理の実装

--- routes/index.js
+++ routes/index.js
@@ -43,5 +43,6 @@ router.post("/", function (req, res, next) {
 });
 
 router.use('/signup', require('./signup'))
+router.use('/signin', require('./signin'))
 
 module.exports = router;
---
+++ routes/signin.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../db/knex");
+
+router.get("/", function (req, res, next) {
+  res.render("signin", {
+    title: "Sign in",
+  });
+});
+
+router.post("/", function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  knex("users")
+    .select("*")
+    .where({
+      name: username,
+      password: password
+    })
+    .then((results) => {
+      if (results.length !== 0) {
+        res.redirect("/");
+      } else {
+        res.render("signin", {
+          title: "sign In",
+          errorMessage: ["ユーザー登録がないか、パスワードが誤っています。"],
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+      res.render("signin", {
+        title: "Sign in",
+        errorMessage: [err.sqlMessage],
+        isAuth: false,
+      });
+    })
+});
+
+module.exports = router;
